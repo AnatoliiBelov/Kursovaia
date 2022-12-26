e24b29287fe73aa51b9bc38f6c4c1e390d31039c
AnatoliiBelov <anatoly.belov.ne@gmail.om>
2022-12-26 17:00:58 +0300

    Прогресс на сегодня(1,5 уровня пройдено)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        EmployeeAction.PrintAllEmployees();
+        EmployeeAction.printSumSalary();
+        EmployeeAction.printMinSalary();
+        EmployeeAction.printMaxSalary();
+        EmployeeAction.printFIOEmployees();
+        EmployeeAction.printAverageValueSalaryEmployees();
+        /**Повышенная сложность!*/
+        EmployeeAction.indexingSalary(89);
+        EmployeeAction.PrintAllEmployees();
+        System.out.println("EmployeeAction.searchMinSalaryInDepartment(2) = " + EmployeeAction.searchMinSalaryInDepartment(2));
+        System.out.println("EmployeeAction.searchMaxSalaryInDepartment(3) = " + EmployeeAction.searchMaxSalaryInDepartment(3));
+        System.out.println("EmployeeAction.calculateSalaryInDepartment(5) = " + EmployeeAction.calculateSalaryInDepartment(5));
+        System.out.println("EmployeeAction.calculateAverageValueSalaryEmployeeInDepartment(5) = " + EmployeeAction.calculateAverageValueSalaryEmployeeInDepartment(5));
+EmployeeAction.indexingSalaryInOneDepartment(20,4);
+EmployeeAction.PrintAllEmployees();
 
     }
+
+
 }
